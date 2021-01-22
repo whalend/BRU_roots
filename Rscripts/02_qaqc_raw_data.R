933850f44c62f63ad10a2b2f95fe8233edfe4fe3
whalend <whalendillon@gmail.com>
2021-01-22 14:03:08 -0500

    Add check of root length.

--- Rscripts/02_qaqc_raw_data.R
+++ Rscripts/02_qaqc_raw_data.R
@@ -4,10 +4,10 @@
 
 library(tidyverse)
 
-df <- read_csv("raw_data/all_raw_observations.csv")
-# source("Rscripts/01_combine_raw_data.R")
+## Make a function to check for duplicate observation identifiers
+## Prints a message if duplicate observations are detected
+## Returns a data frame without duplicate observations regardless
 
-## Check for duplicate observation identifiers
 dupe_check <- function(df){
   ## Create data frame of duplicated observations
   dup_oid = df %>% filter(duplicated(obs_ID)) %>% 
@@ -31,4 +31,17 @@ dupe_check <- function(df){
 
 no_dupes <- dupe_check(df)
 
-## 
+## Check range of values for 'Alive', 'Dead', 'Gone'
+## What are maximum acceptable root lengths within a location?
+## If length is too long presumably SA, Volume, AvgDiam are also questionable
+no_dupes %>% 
+  select(AliveLength_mm, DeadLength_mm, GoneLength_mm) %>% 
+  summary()
+
+no_dupes %>% 
+  filter(AliveLength_mm > 40) %>% # ~3x the diagonal viewing window
+  write_csv("QAQC_intermediates/Alive_over_40mm.csv")
+## send these data to Stacy to check if digitized values are correct
+
+# no_dupes %>% 
+#   filter(AliveLength_mm > 28.3)# ~2x the diagonal viewing window
