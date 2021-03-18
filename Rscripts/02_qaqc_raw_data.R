d8e422a21a7a6c1fbd43904d69030438bfd237ad
whalend <whalendillon@gmail.com>
2021-03-18 13:08:36 -0400

    Handle stupidness with numeric matching. Add a rowID character column. Do rounding of numeric QAQC data - see line 121+

--- Rscripts/02_qaqc_raw_data.R
+++ Rscripts/02_qaqc_raw_data.R
@@ -8,7 +8,7 @@ library(tidyverse)
 library(readxl)
 
 # source("Rscripts/01_combine_raw_data.R")# outputs file at directory below
-df <- read_csv("data/raw_data/all_raw_observations.csv")
+df <- read_csv("data/raw_data/all_raw_observations.csv", trim_ws = TRUE)
 
 ## Make a function to check for duplicate observation identifiers
 ## Prints a message indicating whether duplicate observations are detected
@@ -49,7 +49,7 @@ dupe_check <- function(df, write_path){
 
 ## not the most efficient implementation...
 
-## Make long data frame by root status: Alive, Dead, Gone measurements
+## Add root status column: Alive, Dead, Gone measurements
 munge_long <- function(df){
   A = df %>% 
     # filter(AliveLength_mm > 0) %>% 
@@ -75,6 +75,12 @@ munge_long <- function(df){
 }
 df <- munge_long(df)
 
+df <- df %>% 
+  mutate(rowID = paste0(obs_ID, Length_mm, ProjArea_mm2, SurfArea_mm2,
+                        AvgDiam_mm, Volume_mm3, TipDiam, TipLivStatus,
+                        HighestOrder, ExtPathLength, Altitude,
+                        root_status))
+
 ## Check for Duplicated Observation IDs 'obs_ID'
 ## For 2020 data a root should have only one category
 w_path <- "QAQC_intermediates/"
@@ -85,33 +91,84 @@ dupes <- dupe_check(df, w_path)
 ## Make Data Corrections (initial iteration) ####
 ## Load checked duplicates file from Stacy
 ## Downloaded from Teams
-qaqc_data <- read_xlsx("QAQC_intermediates/QAQC_notes_corr.xlsx", sheet = 1)
-unique(qaqc_data$note)
+qaqc_data <- read_xlsx("QAQC_intermediates/QAQC_notes_corr.xlsx", sheet = "original_notes", trim_ws = TRUE)
 
 ## Check 'notes' values to clue in on anything other than deleting rows
 unique(qaqc_data$note)
 # [3] "need to change data due to reconnecting broken segment link"
 ## deal with this one next
 
-## Filter to duped rows that simply need deleted
-del_dupes <- qaqc_data %>% 
-  filter(note == "delete") %>% 
-  select(-note, AvgDiam_mm = AvgDiam_cm, TotAvgDiam_mm = TotAvgDiam_cm) %>% 
+qaqc_data2 <- read_xlsx("QAQC_intermediates/duped_observations_2021-03-05_with_corrections.xlsx", sheet = "corrected data", trim_ws = TRUE)
+unique(qaqc_data2$flag)
+# view(qaqc_data2)
+
+unique(names(qaqc_data)%in%names(qaqc_data2))
+
+corrections2 <- qaqc_data2 %>% 
+  filter(!flag%in%c("ok","delete"))
+
+qaqc_data = qaqc_data %>% 
+  rename(flag = note, 
+         AvgDiam_mm = AvgDiam_cm,
+         TotAvgDiam_mm = TotAvgDiam_cm) %>% 
   mutate(AvgDiam_mm = AvgDiam_mm/10,
          TotAvgDiam_mm = TotAvgDiam_mm/10)
+# View(qaqc_data)
+
+unique(names(qaqc_data)%in%names(qaqc_data2))
+qaqc_data <- rbind(qaqc_data, qaqc_data2)
+
+## Had a rounding/digits issue crop up in the file transfers/formatting
+## of the qaqc_data2 that produced a bunch of trailing 9s for some of
+## the AvgDiam_mm values, which messed up the matching for row deletion.
+## My solution here is to round at least 'AvgDiam_mm' but perhaps
+## numerics in general to fix this potential issue. Then I also add a
+## 'rowID' column that is a concatenation of obsID and measurements
+## through 'root_status'.
+
+qaqc_data <- qaqc_data %>% 
+  mutate(across(.cols = where(is.numeric),
+                # rounded to 5 digits b/c raw data but thats a lot
+                # of false precision I think
+                .fns = ~ round(.x, digits = 5)
+                )
+         )
+# View(qaqc_data)
+
+qaqc_data <- qaqc_data %>% 
+  mutate(rowID = paste0(obs_ID, Length_mm, ProjArea_mm2, SurfArea_mm2,
+                      AvgDiam_mm, Volume_mm3, TipDiam, TipLivStatus,
+                      HighestOrder, ExtPathLength, Altitude,
+                      root_status))
+
+## Filter to duped rows that simply need deleted
+del_dupes <- qaqc_data %>% 
+  filter(flag == "delete") %>% 
+  select(-flag)
+# view(del_dupes)
 
 ## Keep only the 'good' rows
-df <- anti_join(df, del_dupes)# drops rows in 'del_dupes'
+nrow(df) - nrow(del_dupes)
+df <- anti_join(df, del_dupes, by = "rowID")# drops rows in 'del_dupes'
+
+# dftmp %>% filter(duplicated(obs_ID)) %>% view
+# tmp <- dftmp %>% filter(duplicated(obs_ID))
+# dftmp %>% filter(obs_ID%in%tmp$obs_ID) %>% view()
 
 
 ## Correct data for erroneous observations ####
 ## Stacy made a sheet with corrected data, handling extreme values and any
 ## errors discovered while checking the duplicated 'obs_ID'
-correct_data <- read_xlsx("QAQC_intermediates/QAQC_notes_corr.xlsx", sheet = 2)
+correct_data <- read_xlsx("QAQC_intermediates/QAQC_notes_corr.xlsx", sheet = "corrections")
 correct_data <- correct_data %>% 
   select(-corr_date, AvgDiam_mm = AvgDiam_cm, TotAvgDiam_mm = TotAvgDiam_cm) %>% 
   mutate(AvgDiam_mm = AvgDiam_mm/10,
          TotAvgDiam_mm = TotAvgDiam_mm/10)
+
+corrections2
+correct_data
+correct_data <- rbind(select(corrections2, -flag),
+                      correct_data)
 
 df <- df %>% 
   # First delete the observation(s)
@@ -119,11 +176,13 @@ df <- df %>%
   # Then add the correct data
   union_all(., correct_data)
 
+
 ## Ultimately should have no more duplicate observations, but this is iterative
 ## b/c not all data is present yet. Will need to rerun code above when new data
 ## is added.
 dupes <- dupe_check(df, w_path)
-
+dupes
+# qaqc_data %>% filter(obs_ID%in%dupes$obs_ID) %>% View
 
 ## Check range of values for 'Alive', 'Dead', 'Gone' ####
 ## What are maximum acceptable root lengths and diameters within a location?
